243ae3c4c4c94f6fb0fc6be8b44b68fbdd17ad4d
lyannaguo <jg1347@scarletmail.rutgers.edu>
2020-02-20 20:47:44 -0500

    finish

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+
+    public GameObject Projectile;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Projectile.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Projectile.transform.position + offset;
+    }
+}
